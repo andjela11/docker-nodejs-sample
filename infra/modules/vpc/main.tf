0e8a00f4cdb02315231498c9904c3741c9508c2f
andjela.jovanovic <andjela1198@gmail.com>
2024-05-16 15:28:39 +0200

    add dynamic subnet calculation

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -1,13 +1,57 @@
+module "subnet_addrs" {
+  source = "hashicorp/subnets/cidr"
+
+  base_cidr_block = module.vpc.vpc_cidr_block
+  networks = [
+    {
+      name     = "subnet-private-1A"
+      new_bits = 2
+    },
+    {
+      name     = "subnet-private-1B"
+      new_bits = 2
+    },
+     {
+      name     = "subnet-private-1C"
+      new_bits = 2
+    },
+     {
+      name     = "subnet-public-1A"
+      new_bits = 8
+    },
+    {
+      name     = "subnet-public-1B"
+      new_bits = 8
+    },
+    {
+      name     = "subnet-public-1C"
+      new_bits = 8
+    },
+    {
+      name     = "subnet-db-1A"
+      new_bits = 8
+    },
+      {
+      name     = "subnet-db-1B"
+      new_bits = 8
+    },
+      {
+      name     = "subnet-db-1C"
+      new_bits = 8
+    },
+  ]
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "${var.name}-vpc"
   cidr = var.vpc_cidr_block
 
-  azs             = data.aws_availability_zones.available.names
-  private_subnets = var.private_subnet_cidr
-  public_subnets  = var.public_subnet_cidr
-  database_subnets = var.db_subnet_cidr
+  azs = data.aws_availability_zones.available.names
+  private_subnets = [lookup(module.subnet_addrs.network_cidr_blocks, "subnet-private-1A", "default"),lookup(module.subnet_addrs.network_cidr_blocks, "subnet-private-1B", "default"), lookup(module.subnet_addrs.network_cidr_blocks, "subnet-private-1C", "default")]
+  public_subnets = [lookup(module.subnet_addrs.network_cidr_blocks, "subnet-public-1A", "default"),lookup(module.subnet_addrs.network_cidr_blocks, "subnet-public-1B", "default"), lookup(module.subnet_addrs.network_cidr_blocks, "subnet-public-1C", "default")]
+  database_subnets = [lookup(module.subnet_addrs.network_cidr_blocks, "subnet-db-1A", "default"),lookup(module.subnet_addrs.network_cidr_blocks, "subnet-db-1B", "default"), lookup(module.subnet_addrs.network_cidr_blocks, "subnet-db-1C", "default")]
 
   enable_nat_gateway = true
   single_nat_gateway = true
--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -3,24 +3,6 @@ variable "vpc_cidr_block" {
   default = "172.17.0.0/16"
 }
 
-variable "private_subnet_cidr" {
-  type = list(string)
-  description = "Private Subnet CIDR values"
-  default = [ "172.17.0.0/18", "172.17.64.0/18", "172.17.128.0/18" ]
-}
-
-variable "public_subnet_cidr" {
-  type = list(string)
-  description = "Public Subnet CIDR values"
-  default = [ "172.17.192.0/24", "172.17.193.0/24", "172.17.194.0/24" ]
-}
-
-variable "db_subnet_cidr" {
-  type = list(string)
-  description = "DB Subnet CIDR values"
-  default = [ "172.17.195.0/24", "172.17.196.0/24", "172.17.197.0/24" ]
-}
-
 variable "Owner" {
     type = string
     default = "Andjela Jovanovic"
